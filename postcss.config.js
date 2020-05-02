3aba046bac3f89f800e81743555b611072d8f997
Conor Dockry <conordockry@gmail.com>
2020-05-01 21:17:56 -0400

    use tailwind native purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,3 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: [
-                './src/pages/**/*.{js,jsx,ts,tsx}',
-                './src/components/**/*.{js,jsx,ts,tsx}',
-              ],
-              defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
-              whitelistPatterns: [/data-reach/],
-              whitelistPatternsChildren: [/data-reach/],
-            },
-          ],
-        ]
-      : []),
-    'postcss-preset-env',
-  ],
+  plugins: ['tailwindcss', 'postcss-preset-env'],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  purge: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
