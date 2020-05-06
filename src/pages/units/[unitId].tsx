fb29d3d8db57d98044dab1c9f5359c869b2f4e3d
Conor Dockry <conordockry@gmail.com>
2020-05-05 21:35:23 -0400

    fix wrong query param reference in unit

--- src/pages/units/[unitId].tsx
+++ src/pages/units/[unitId].tsx
@@ -3,15 +3,19 @@ import { useRouter } from 'next/router'
 import { Layout } from 'components'
 import { fetchGuard } from 'client'
 
-async function fetchUnit(key, unitId: string): Promise<Unit | undefined> {
+async function fetchUnit(
+  key,
+  unitId: string | string[]
+): Promise<Unit | undefined> {
   return fetchGuard<Unit>(`/api/units/${unitId}`)
 }
 
 export default function Unit() {
-  const {
-    query: { unitId },
-  } = useRouter()
-  const { data: unit } = useQuery(['units', unitId!.toString()], fetchUnit)
+  const { query } = useRouter()
+  const { data: unit } = useQuery(
+    query.unitId ? ['units', query.unitId] : null,
+    fetchUnit
+  )
   return (
     <Layout>
       <h1 className="m-0 py-8 text-3xl">Unit: {unit ? unit.name : ''}</h1>
