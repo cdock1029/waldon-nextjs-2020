caaf9357af481d1a726d4436befe911e398ad6bf
Conor Dockry <conordockry@gmail.com>
2020-05-05 22:35:26 -0400

    add page title in Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import 'styles/index.css'
+import Head from 'next/head'
 import { PropertyProvider } from 'client'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PropertyProvider>
-      <Component {...pageProps} />
-    </PropertyProvider>
+    <>
+      <Head>
+        <title>Waldon</title>
+      </Head>
+      <PropertyProvider>
+        <Component {...pageProps} />
+      </PropertyProvider>
+    </>
   )
 }
 
